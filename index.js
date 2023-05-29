9bd2942b9363d7e2984c73c0d1e98a1e459bd43f
yehoki <patsenn2000@gmail.com>
2023-05-29 13:53:53 +0100

    build

--- index.js
+++ index.js
@@ -66,6 +66,12 @@ app.get(`/api/persons/:id`, (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
 const findFirstFreeId = () => {
   if (persons.length === 0) {
     return 1;
