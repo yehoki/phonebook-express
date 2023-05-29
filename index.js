d96d05a1cea75e4cd4991830afe71201a7c3962b
yehoki <patsenn2000@gmail.com>
2023-05-29 12:59:21 +0100

    Home GET test

--- index.js
+++ index.js
@@ -4,8 +4,37 @@ const PORT = 3000;
 const app = express();
 app.use(express.json());
 
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
 app.get("/", (req, res) => {
-  return res.send("Hello matey");
+  const time = new Date();
+  const info = persons.length;
+  return res.send(`
+  Phonebook has info for ${info} people
+  <br/>
+  ${time}`);
 });
 
 app.listen(PORT, () => {
